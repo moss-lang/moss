d20ca3453d52f77d6f08e21bad2f8dd91b049b24
Sam Estep <sam@samestep.com>
2025-08-29 12:41:33 -0400

    Publish to crates.io

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      fenix,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell = pkgs.mkShellNoCC {
+          buildInputs = [
+            pkgs.nixfmt
+            (fenix.packages.${system}.stable.toolchain)
+          ];
+        };
+      }
+    );
+}
