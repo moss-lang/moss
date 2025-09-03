26df1901d2ddcf28f4ced3ca68ad41dcde871303
Sam Estep <sam@samestep.com>
2025-09-03 10:39:56 -0400

    Handle shebangs

--- crates/connie-cli/src/bin/connie.rs
+++ crates/connie-cli/src/bin/connie.rs
@@ -15,6 +15,14 @@ enum Commands {
 }
 
 fn main() -> ExitCode {
+    // Handle shebangs.
+    if let Some((file, args)) = std::env::args().skip(1).collect::<Vec<_>>().split_first() {
+        // Prevent collision with possible future subcommands.
+        if file.contains("/") {
+            println!("{file} {args:?}");
+            return ExitCode::SUCCESS;
+        }
+    }
     let args = Cli::parse();
     match args.command {
         Commands::Lsp => connie_cli::lsp::language_server(),
