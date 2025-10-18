66af6c7ed30167c102b6c7742247f42b428888fd
Sam Estep <sam@samestep.com>
2025-10-18 16:56:38 -0400

    Start dealing with scopes in lowering

--- crates/connie/src/lower.rs
+++ crates/connie/src/lower.rs
@@ -1,10 +1,11 @@
 use index_vec::{IndexVec, define_index_type};
+use indexmap::IndexMap;
 use logos::Logos;
 
 use crate::{
     intern::{StrId, Strings},
     lex::{Token, TokenId, TokenStarts},
-    parse::{Region, RegionId, Tree},
+    parse::{self, Ctx, Region, RegionId, Tree},
     util::IdRange,
 };
 
@@ -27,8 +28,6 @@ define_index_type! {
 define_index_type! {
     pub struct FuncId = u32;
 }
-
-pub type Path = Option<(PathId, StrId)>;
 
 #[derive(Clone, Copy, Debug)]
 pub enum Type {
@@ -58,13 +57,23 @@ pub struct Func {
 #[derive(Debug, Default)]
 pub struct IR {
     pub strings: Strings,
-    pub paths: IndexVec<PathId, Path>,
     pub types: IndexVec<TypeId, Type>,
     pub vars: IndexVec<VarId, Var>,
     pub vals: IndexVec<InstrId, TypeId>,
     pub instrs: IndexVec<InstrId, Instr>,
     pub funcs: IndexVec<FuncId, Func>,
     pub main: Option<FuncId>,
+}
+
+type Path = Option<(PathId, StrId)>;
+
+#[derive(Clone, Copy, Debug)]
+enum Named {
+    Scope,
+    Type(TypeId),
+    Var(VarId),
+    Val(InstrId),
+    Func(InstrId),
 }
 
 pub enum LowerError {}
@@ -76,6 +85,8 @@ struct Lower<'a> {
     starts: &'a TokenStarts,
     tree: &'a Tree,
     ir: IR,
+    paths: IndexMap<Path, Named>,
+    path: PathId,
 }
 
 impl<'a> Lower<'a> {
@@ -88,11 +99,37 @@ impl<'a> Lower<'a> {
         &self.source[(range.start + start)..(range.end + start)]
     }
 
-    fn region(&self, id: RegionId) {
+    fn string(&mut self, token: TokenId) -> StrId {
+        self.ir.strings.make(self.slice(token))
+    }
+
+    fn set(&mut self, token: TokenId, named: Named) -> PathId {
+        let name = self.string(token);
+        let (i, _) = self.paths.insert_full(Some((self.path, name)), named);
+        PathId::from_usize(i)
+    }
+
+    fn set_scope(&mut self, token: TokenId) -> PathId {
+        self.set(token, Named::Scope)
+    }
+
+    fn set_var(&mut self, token: TokenId, id: VarId) -> PathId {
+        self.set(token, Named::Var(id))
+    }
+
+    fn region(&mut self, id: RegionId) {
         let Region { ctxs, regions, .. } = self.tree.regions[id];
         for ctx in ctxs {
-            for val in self.tree.ctxs[ctx].vals {
-                // TODO
+            let Ctx { name, vals } = self.tree.ctxs[ctx];
+            let scope = self.set_scope(name);
+            for val in vals {
+                let ty = match self.tree.types[self.tree.vals[val].ty] {
+                    parse::Type::Name(name) => match self.slice(name) {
+                        "String" => Type::String,
+                        _ => todo!(),
+                    },
+                };
+                self.ir.types.push(ty);
             }
         }
         for region in regions {
@@ -100,7 +137,7 @@ impl<'a> Lower<'a> {
         }
     }
 
-    fn program(self) -> LowerResult<IR> {
+    fn program(mut self) -> LowerResult<IR> {
         self.region(self.tree.root);
         // TODO
         Ok(self.ir)
@@ -108,11 +145,15 @@ impl<'a> Lower<'a> {
 }
 
 pub fn lower(source: &str, starts: &TokenStarts, tree: &Tree) -> LowerResult<IR> {
+    let mut paths = IndexMap::new();
+    let (i, _) = paths.insert_full(None, Named::Scope);
     Lower {
         source,
         starts,
         tree,
         ir: IR::default(),
+        paths,
+        path: PathId::from_usize(i),
     }
     .program()
 }
