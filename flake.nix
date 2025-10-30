8a22e9cad4faac6ff7373d3eaf2e1df0bdad31fd
Sam Estep <sam@samestep.com>
2025-10-30 08:50:38 -0400

    Add a package to the flake

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    crane.url = "github:ipetkov/crane";
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -12,6 +13,7 @@
       self,
       nixpkgs,
       flake-utils,
+      crane,
       rust-overlay,
     }:
     flake-utils.lib.eachDefaultSystem (
@@ -21,8 +23,20 @@
           inherit system;
           overlays = [ (import rust-overlay) ];
         };
+        craneLib = crane.mkLib pkgs;
+        commonArgs = {
+          pname = "connie";
+          src = craneLib.cleanCargoSource ./.;
+          strictDeps = true;
+        };
       in
       {
+        packages.default = craneLib.buildPackage (
+          commonArgs
+          // {
+            cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+          }
+        );
         devShells.default = pkgs.mkShellNoCC {
           buildInputs = [
             # Necessary tools.
