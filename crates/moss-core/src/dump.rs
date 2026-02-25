6cb732b975c4e33bde7bfa731acfc4b16ea3d2cf
Sam Estep <sam@samestep.com>
2026-02-25 15:21:22 -0500

    Print unfinished instruction bodies at the end

--- crates/moss-core/src/dump.rs
+++ crates/moss-core/src/dump.rs
@@ -1,114 +1,174 @@
 use std::collections::HashMap;
 
+use index_vec::{IndexVec, index_vec};
+
 use crate::{
     intern::StrId,
-    lower::{IR, Instr, ModuleId, Named, Names, Tydef},
+    lower::{Body, IR, Instr, InstrId, ModuleId, Named, Names, Tydef},
 };
 
-pub fn dump(ir: &IR, names: &Names) {
-    let mut defined_in = HashMap::<Named, (ModuleId, StrId)>::new();
-    let mut by_module = HashMap::<ModuleId, Vec<Named>>::new();
-    for (&(module, name), &named) in &names.names {
-        defined_in.entry(named).or_insert((module, name));
-        by_module.entry(module).or_default().push(named);
+struct Dump<'a> {
+    ir: &'a IR,
+    names: &'a Names,
+    source: HashMap<Named, (ModuleId, StrId)>,
+    printed: IndexVec<InstrId, bool>,
+}
+
+impl<'a> Dump<'a> {
+    fn named_raw(&self, named: Named, name: StrId) -> Option<Body> {
+        match named {
+            Named::Module(_) => todo!(),
+            Named::Tydef(def) => {
+                print!("type #{} \"{}\"", def.index(), &self.ir.strings[name]);
+                let Tydef(body) = self.ir.tydefs[def];
+                Some(body)
+            }
+            Named::Tagdef(def) => todo!(),
+            Named::Aliasdef(def) => todo!(),
+            Named::Sigdef(def) => todo!(),
+            Named::Fndef(def) => todo!(),
+            Named::Valdef(def) => todo!(),
+            Named::Ctxdef(def) => todo!(),
+        }
     }
 
-    let mut first = true;
-    for (module, _) in ir.modules.iter_enumerated() {
-        if !first {
-            println!();
-        }
-        first = false;
+    fn named(&self, named: Named) -> Option<Body> {
+        let (_, name) = self.source[&named];
+        self.named_raw(named, name)
+    }
 
-        println!("module #{} {{", module.index());
+    fn instr(&mut self, instr: InstrId) {
+        print!("    %{} = ", instr.index());
+        if self.printed[instr] {
+            println!("duplicate");
+            return;
+        }
+
+        self.printed[instr] = true;
+        match self.ir.instrs[instr] {
+            Instr::Lambda => print!("lambda {{"),
+            Instr::EndLambda { result } => todo!(),
+            Instr::Apply { lambda, args } => todo!(),
+            Instr::Stack { items } => {
+                print!("stack");
+
+                let mut first = true;
+                for item in items {
+                    if !first {
+                        print!(",");
+                    }
+                    first = false;
+
+                    print!(" %{}", self.ir.items[item].index());
+                }
+            }
+            Instr::NeedTydef { def, param } => todo!(),
+            Instr::NeedSigdef { def, param } => todo!(),
+            Instr::NeedValdef { def, param } => todo!(),
+            Instr::NeedCtxdef { def, param } => todo!(),
+            Instr::Tagdef { def } => todo!(),
+            Instr::Aliasdef { def } => todo!(),
+            Instr::Tuple { elems } => todo!(),
+            Instr::Record { fields } => todo!(),
+            Instr::Context => todo!(),
+            Instr::Fndef { def } => todo!(),
+            Instr::Get { ctx, slot } => todo!(),
+            Instr::Lit { val } => todo!(),
+            Instr::Bind { args, bind } => todo!(),
+            Instr::BindTydef { def, bind } => todo!(),
+            Instr::BindSigdef { def, bind } => todo!(),
+            Instr::BindValdef { def, bind } => todo!(),
+            Instr::BindCtxdef { def, bind } => todo!(),
+            Instr::Sig { param, result } => todo!(),
+            Instr::Set { lhs, rhs } => todo!(),
+            Instr::If { ty, cond } => todo!(),
+            Instr::Else { result } => todo!(),
+            Instr::EndIf { result } => todo!(),
+            Instr::Loop => todo!(),
+            Instr::EndLoop => todo!(),
+            Instr::Br { depth } => todo!(),
+            Instr::Expr { ty, expr } => todo!(),
+        }
+        println!();
+    }
+
+    fn body(&mut self, body: Body) {
+        for instr in body.body {
+            self.instr(instr);
+        }
+        println!("    return %{}", body.result().index());
+        println!("  }}");
+    }
+
+    fn program(&mut self) {
+        self.source = HashMap::<Named, (ModuleId, StrId)>::new();
+        let mut by_module = HashMap::<ModuleId, Vec<Named>>::new();
+        for (&(module, name), &named) in &self.names.names {
+            self.source.entry(named).or_insert((module, name));
+            by_module.entry(module).or_default().push(named);
+        }
 
         let mut first = true;
-        for &named in by_module.get(&module).into_iter().flatten() {
-            let (original, name) = defined_in[&named];
-            if module != original {
-                continue;
-            }
-
+        for (module, _) in self.ir.modules.iter_enumerated() {
             if !first {
                 println!();
             }
             first = false;
 
-            match named {
-                Named::Module(_) => todo!(),
-                Named::Tydef(def) => {
-                    println!("  type #{} \"{}\" {{", def.index(), &ir.strings[name]);
-                    let Tydef(body) = ir.tydefs[def];
-                    for instr in body.body {
-                        print!("    %{} = ", instr.index());
-                        match ir.instrs[instr] {
-                            Instr::Lambda => todo!(),
-                            Instr::EndLambda { result } => todo!(),
-                            Instr::Apply { lambda, args } => todo!(),
-                            Instr::Stack { items } => {
-                                print!("stack");
+            print!("module #{} {{", module.index());
 
-                                let mut first = true;
-                                for item in items {
-                                    if !first {
-                                        print!(",");
-                                    }
-                                    first = false;
-
-                                    print!(" %{}", ir.items[item].index());
-                                }
-
-                                println!();
-                            }
-                            Instr::NeedTydef { def, param } => todo!(),
-                            Instr::NeedSigdef { def, param } => todo!(),
-                            Instr::NeedValdef { def, param } => todo!(),
-                            Instr::NeedCtxdef { def, param } => todo!(),
-                            Instr::Tagdef { def } => todo!(),
-                            Instr::Aliasdef { def } => todo!(),
-                            Instr::Tuple { elems } => todo!(),
-                            Instr::Record { fields } => todo!(),
-                            Instr::Context => todo!(),
-                            Instr::Fndef { def } => todo!(),
-                            Instr::Get { ctx, slot } => todo!(),
-                            Instr::Lit { val } => todo!(),
-                            Instr::Bind { args, bind } => todo!(),
-                            Instr::BindTydef { def, bind } => todo!(),
-                            Instr::BindSigdef { def, bind } => todo!(),
-                            Instr::BindValdef { def, bind } => todo!(),
-                            Instr::BindCtxdef { def, bind } => todo!(),
-                            Instr::Sig { param, result } => todo!(),
-                            Instr::Set { lhs, rhs } => todo!(),
-                            Instr::If { ty, cond } => todo!(),
-                            Instr::Else { result } => todo!(),
-                            Instr::EndIf { result } => todo!(),
-                            Instr::Loop => todo!(),
-                            Instr::EndLoop => todo!(),
-                            Instr::Br { depth } => todo!(),
-                            Instr::Expr { ty, expr } => todo!(),
-                        }
-                    }
-                    println!("    return %{}", body.result().index());
-                    println!("  }}");
+            for &named in by_module.get(&module).into_iter().flatten() {
+                let (original, name) = self.source[&named];
+                if module != original {
+                    continue;
                 }
-                Named::Tagdef(def) => todo!(),
-                Named::Aliasdef(def) => todo!(),
-                Named::Sigdef(def) => todo!(),
-                Named::Fndef(def) => todo!(),
-                Named::Valdef(def) => todo!(),
-                Named::Ctxdef(def) => todo!(),
+
+                println!();
+                print!("  ");
+                let contents = self.named_raw(named, name);
+                println!(" {{");
+                if let Some(body) = contents {
+                    self.body(body);
+                }
             }
+
+            println!("}}");
         }
 
-        println!("}}");
-    }
+        let mut prev = None;
+        for (instr, _) in self.ir.instrs.iter_enumerated() {
+            if self.printed[instr] {
+                continue;
+            }
 
-    if !first {
-        println!();
-    }
-    first = false;
-    println!("unfinished {{");
-    println!("}}");
+            match prev {
+                None => {
+                    if !first {
+                        println!();
+                    }
+                    first = false;
 
-    let _ = first;
+                    println!("unfinished {{");
+                    println!("  {{");
+                }
+                Some(_) => todo!(),
+            }
+            self.instr(instr);
+            prev = Some(instr);
+        }
+        if prev.is_some() {
+            println!("  }}");
+            println!("}}");
+        }
+    }
+}
+
+pub fn dump(ir: &IR, names: &Names) {
+    Dump {
+        ir,
+        names,
+        source: HashMap::new(),
+        printed: index_vec![false; ir.instrs.len()],
+    }
+    .program();
 }
