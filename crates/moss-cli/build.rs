b0451010a11194956e898e93bc0ec63695a424d2
Sam Estep <sam@samestep.com>
2026-07-29 04:16:15 +0000

    Add Binaryen rpath on macOS

--- crates/moss-cli/build.rs
+++ crates/moss-cli/build.rs
@@ -5,20 +5,29 @@ fn main() {
     println!("cargo:rerun-if-env-changed=BINARYEN_STATIC");
     println!("cargo:rerun-if-env-changed=BINARYEN_STATIC_STDCPP");
 
-    if let Some(dir) = env::var_os("BINARYEN_LIB_DIR") {
+    let binaryen_lib_dir = env::var_os("BINARYEN_LIB_DIR");
+    if let Some(dir) = &binaryen_lib_dir {
         println!(
             "cargo:rustc-link-search=native={}",
             Path::new(&dir).display()
         );
     }
     let static_binaryen = env::var("BINARYEN_STATIC").is_ok_and(|value| value != "0");
+    let target_os = env::var("CARGO_CFG_TARGET_OS").unwrap_or_default();
+    if target_os == "macos" && !static_binaryen {
+        if let Some(dir) = &binaryen_lib_dir {
+            println!(
+                "cargo:rustc-link-arg=-Wl,-rpath,{}",
+                Path::new(dir).display()
+            );
+        }
+    }
     println!(
         "cargo:rustc-link-lib={}binaryen",
         if static_binaryen { "static=" } else { "" }
     );
 
     if env::var("CARGO_CFG_TARGET_ENV").as_deref() != Ok("msvc") {
-        let target_os = env::var("CARGO_CFG_TARGET_OS").unwrap_or_default();
         let cpp = if target_os == "macos" {
             "c++"
         } else {
