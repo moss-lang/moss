172f0ecf847f14a5822d23e278fd0d99f4791a52
Sam Estep <sam@samestep.com>
2026-08-05 13:07:46 -0400

    Switch from `nixos-26.05` to `nixpkgs-unstable`

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
